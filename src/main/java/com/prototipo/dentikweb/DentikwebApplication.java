579a11e6a0ff2e69ee53df4b8e6cf8c2e6921028
JavierJadan <javierjadan6@gmail.com>
2026-07-22 16:58:31 -0500

    Initial commit: Spring Boot project

---
+++ src/main/java/com/prototipo/dentikweb/DentikwebApplication.java
@@ -0,0 +1,13 @@
+package com.prototipo.dentikweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DentikwebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DentikwebApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/prototipo/dentikweb/DentikwebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.prototipo.dentikweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DentikwebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
