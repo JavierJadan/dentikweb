75b25dcc9eeae428ed53f8f077882c3ea6d47a16
JavierJadan <javierjadan6@gmail.com>
2026-07-22 21:40:51 -0500

    Nuevos cambios en plantilla layout

---
+++ src/main/java/com/prototipo/dentikweb/controller/UsuarioController.java
@@ -0,0 +1,17 @@
+package com.prototipo.dentikweb.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/usuarios")
+public class UsuarioController {
+	
+	@GetMapping
+    public String leerPagina() {
+        return "/usuario/listarusuario";
+    }
+
+
+}
